304bcc43a3d7719007a96fb200f39f412ceedcda
Andi Dittrich <andi@andidittrich.de>
2021-01-29 16:11:33 +0100

    added sample application

---
+++ examples/app.js
@@ -0,0 +1,56 @@
+const _loggingFacility = require('logging-facility');
+const _path = require('path');
+const _koaMagic = require('../koa-magic');
+
+// logging
+_loggingFacility.setBackend('fancy-cli');
+
+// initialize middleware
+function applicationHandler(koa){
+    // trust proxy field
+    koa.proxy = true;
+
+    // security
+    koa.attach(function(ctx, next){
+        ctx.set('X-Powered-By', 'lighttpd/aenon');
+
+        // security
+        ctx.set('X-XSS-Protection', '1; mode=block');
+        ctx.set('X-Content-Type-Options', 'nosniff');
+        ctx.set('X-Frame-Options', 'DENY');
+
+        return next();
+    });
+
+    // static content with and without prefix
+    koa.attach(/^(\/[a-z0-9]{12}|)/i, _koaMagic.StaticFiles({
+        root: _path.join(__dirname, 'public'),
+        maxAge: 1000*60*60*24*180,
+        index: false
+    }));
+
+    koa.get('/hello/:id', (ctx) => {
+        ctx.body = "hello world - " + ctx.params.id;
+    });
+}
+
+function configInitialization(){
+    
+    // webserver config
+    const config = {
+        listen: '0.0.0.0',
+        port: 8888,
+        keygrip: ['3nfoihf928fg29fg23'],
+        session: {
+            key: 'pmc-gc-webapp',
+            maxAge: 86400000
+        },
+        httpErrorPages: {
+            footer: 'MyFooter',
+        }
+    };
+
+    return config;
+}
+
+_koaMagic.ApplicationServer(configInitialization, applicationHandler).init();
